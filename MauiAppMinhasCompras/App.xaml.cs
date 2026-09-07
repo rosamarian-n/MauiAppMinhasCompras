1b12ec94575f4b2f61675bd6dd31acf55b2f0507
Rosamaria de Oliveira <160526041+rosamarian-n@users.noreply.github.com>
2026-09-07 09:47:15 -0300

    Agenda-2

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,9 +1,30 @@
-﻿using MauiAppMinhasCompras.Resources.Views;
+﻿using MauiAppMinhasCompras.Properties.Helders;
+using MauiAppMinhasCompras.Resources.Views;
 
 namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        //Apresentação privada do banco de dados no constructor
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get 
+            {
+                if (_db == null)
+                {
+                    string path=Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+                return _db;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
--- MauiAppMinhasCompras/Resources/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Resources/Views/NovoProduto.xaml.cs
@@ -1,3 +1,6 @@
+using MauiAppMinhasCompras.Models;
+using System.Threading.Tasks;
+
 namespace MauiAppMinhasCompras.Resources.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +9,27 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto p = new Produto
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text)
+			};
+
+			await App.Db.Insert(p);
+
+			await DisplayAlert("Sucesso", "Registro inserido com sucesso!", "OK");
+            //Todo metodo que tem await assincrono, ou seja, ele não bloqueia a execução do código
+
+        }
+        catch (Exception ex)
+		{
+			await DisplayAlert("Ops!", ex.Message, "OK");
+        }
+    }
 }
